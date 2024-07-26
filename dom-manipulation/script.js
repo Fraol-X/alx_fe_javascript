7a7d7bf5bdcd6c48344defcdf5b701d4e15ee62e
Fraol-X <pfresh641@gmail.com>
2024-07-26 20:10:26 +0300

    added createElement and appendChild

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,49 +1,57 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
     const quotes = [
-
         {text: "If you can dream it, you can do it.", category:"Motivational"},
-
         {text: "Better to remain silent and be thought a fool than to speak out and remove all doubt.", category: "Funny"},
-
         {text: "Early to bed and early to rise makes a man healthy, wealthy and wise.", category: "Health"},
-
         {text: "I have no special talent. I am only passionately curious.", category: "Educational"},
-
         {text: "One machine can do the work of fifty ordinary men. No machine can do the work of one extraordinary man.", category: "Technology"}
     ];
+
     const quoteDisplay = document.getElementById('quoteDisplay');
     const newQuoteButton = document.getElementById('newQuote');
-    const newQuoteTextInput = document.getElementById('newQuoteText');
-    const newQuoteCategoryInput = document.getElementById('newQuoteCategory');
-    const addQuoteButton = document.getElementById('addQuoteButton');
 
-    function createAddQuoteForm(){
-        const htmlForm = `
-            <div id="addQuoteForm">
-                <input id="newQuoteText" type="text" placeholder="Enter a new quote" />
-                <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
-                <button id="addQuoteButton">Add Quote</button>
-            </div>`;
+    function createAddQuoteForm() {
+        const divForm = document.createElement('div');
+        divForm.id = 'addQuoteForm';
 
-        document.body.insertAdjacentHTML('beforeend', htmlForm);
+        const newQuoteTextInput = document.createElement('input');
+        newQuoteTextInput.id = 'newQuoteText';
+        newQuoteTextInput.type = 'text';
+        newQuoteTextInput.placeholder = 'Enter a new quote';
 
+        const newQuoteCategoryInput = document.createElement('input');
+        newQuoteCategoryInput.id = 'newQuoteCategory';
+        newQuoteCategoryInput.type = 'text';
+        newQuoteCategoryInput.placeholder = 'Enter quote category';
+
+        const addQuoteButton = document.createElement('button');
+        addQuoteButton.id = 'addQuoteButton';
+        addQuoteButton.textContent = 'Add Quote';
+
+        divForm.appendChild(newQuoteTextInput);
+        divForm.appendChild(newQuoteCategoryInput);
+        divForm.appendChild(addQuoteButton);
+
+        document.body.appendChild(divForm);
+
+        addQuoteButton.addEventListener('click', addQuote);
     }
 
     function showRandomQuote() {
-
         const randomDisplay = Math.floor(Math.random() * quotes.length);
         const quote = quotes[randomDisplay];
         quoteDisplay.innerHTML = `<p>Quote: - ${quote.text}</p><p>Category: - ${quote.category}</p>`;
     }
 
     function addQuote() {
+        const newQuoteTextInput = document.getElementById('newQuoteText');
+        const newQuoteCategoryInput = document.getElementById('newQuoteCategory');
 
         const text = newQuoteTextInput.value.trim();
         const category = newQuoteCategoryInput.value.trim();
 
-
         if (text === "" || category === "") {
-            alert("Quote and Category Inputs are Required!")
+            alert("Quote and Category Inputs are Required!");
             return;
         }
 
@@ -52,10 +60,9 @@ document.addEventListener('DOMContentLoaded', function(){
         newQuoteTextInput.value = "";
         newQuoteCategoryInput.value = "";
         showRandomQuote();
-
     }
 
+    createAddQuoteForm();
     newQuoteButton.addEventListener('click', showRandomQuote);
-    addQuoteButton.addEventListener('click' , addQuote);
     showRandomQuote();
 });
